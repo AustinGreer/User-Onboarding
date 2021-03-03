338020aa0c4c3c7a181ef73c1fca9b1e63297205
Austin Greer <agreer.ag12@gmail.com>
2021-03-03 14:08:08 -0500

    built out basic form scaffold

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Form () {
     return (
-        <div>Here are the forms</div>
+        <StyledDiv>
+            <form>
+                <h1>Welcome to User Onboard!</h1>
+                <label>
+                    Name:
+                    <input 
+                        type='text'
+                        name='name'
+                    />
+                </label>
+
+                <label>
+                    Email:
+                    <input
+                        type='email'
+                        name='email'
+                    />
+                </label>
+
+                <label>
+                    Password:
+                    <input 
+                        type='password'
+                        name='password'
+                    />
+                </label>
+
+                <label>
+                    Terms of Service:
+                    <input 
+                        type='checkbox'
+                        name='Terms of Service'
+                    />
+                </label>
+
+                <button type='submit'>Submit!</button>
+            </form>
+        </StyledDiv>
     )
 }
+
+const StyledDiv = styled.div`
+    form {
+        margin-top: 5%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+`
 
 export default Form
